1f43e89a4585b148a4e448704b165b4784295499
SaiManojK16 <skartala@albany.edu>
2025-11-10 15:33:18 -0500

    Set up Next.js app wrapper and document

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Acceptly - From Mistakes to Mastery, One Transition at a Time" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
