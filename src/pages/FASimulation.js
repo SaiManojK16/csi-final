f8529ffcccb453e1d478a076abd710fafd0ba4d9
SaiManojK16 <skartala@albany.edu>
2025-12-02 00:40:29 -0500

    Add Acceptly logo to FASimulation page matching landing page style
    - Replace icon-based logo with text-only Acceptly logo
    - Match Header component logo styling (font-size 22px, font-weight 800, color #1f2a44)
    - Update logo link to navigate to home page (/)
    - Remove unused logo-icon and logo-text-short styles

--- src/pages/FASimulation.js
+++ src/pages/FASimulation.js
@@ -507,14 +507,8 @@ const FASimulation = () => {
       {/* LeetCode Style Header - Problem Navigation */}
       <div className="fa-header-leetcode">
         <div className="fa-header-left">
-          <Link to="/problems" className="header-logo">
-            <span className="logo-icon">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
-                <path d="M12 8v8M8 12h8"/>
-              </svg>
-            </span>
-            <span className="logo-text-short">Acceptly</span>
+          <Link to="/" className="header-logo">
+            <span className="logo-text">Acceptly</span>
           </Link>
           <div className="header-navigation">
             <button 
